5d707240bee0a910a54ee85ab917e824ad5cfe42
Luizenys <luizars@id.uff.br>
2024-03-19 11:05:59 -0300

    chore: project setup

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+    white: '#fff',
+  
+    'gray-100': '#E1E1E6',
+    'gray-300': '#C4C4CC',
+    'gray-400': '#8D8D99',
+    'gray-500': '#7C7C8A',
+    'gray-600': '#323238',
+    'gray-700': '#29292E',
+    'gray-800': '#202024',
+    'gray-900': '#121214',
+  
+    'yellow-300': '#FEEA6B',
+    'yellow-500': '#FEE337',
+    'yellow-700': '#FFDC00',
+  
+    'purple-300': '#BF6EFF',
+    'purple-500': '#A533FF',
+    'purple-700': '#8E00FF',
+  } as const
