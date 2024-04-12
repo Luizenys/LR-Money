53b85f32270c0f10a786ed995d7cf646e6f5b514
Luiza Rodrigues <luizars@id.uff.br>
2024-04-12 15:57:00 -0300

    feat: creating new transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,9 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as z from 'zod';
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
+import { api } from '../../lib/axios';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
@@ -15,7 +18,9 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-    const { control, register, handleSubmit, formState: { isSubmitting } } = useForm<NewTransactionFormInputs>({
+    const { createTransaction } = useContext(TransactionsContext);
+
+    const { reset, control, register, handleSubmit, formState: { isSubmitting } } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema),
         defaultValues: {
             type: 'income'
@@ -23,8 +28,8 @@ export function NewTransactionModal() {
     })
 
     async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        console.log(data);
+        await createTransaction(data);
+        reset();
     }
     return (
         <Dialog.Portal>
