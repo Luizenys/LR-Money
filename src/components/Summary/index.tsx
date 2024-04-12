1358a19f2d2a258feb6f7097669f3c3094a31cc2
Luiza Rodrigues <luizars@id.uff.br>
2024-04-12 10:49:09 -0300

    feat: implementing useSummary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
 import { SummaryCard, SummaryContainer } from "./styles"
-import { TransactionsContext } from "../../contexts/TransactionsContext"
-import { useContext } from "react"
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext)
+    const summary = useSummary();
     
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income') {
-                acc.income += transaction.price;
-                acc.total += transaction.price;
-            } else {
-                acc.outcome += transaction.price;
-                acc.total -= transaction.price;
-            }
-            return acc;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    )
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -46,7 +28,7 @@ export function Summary() {
 
             <SummaryCard variant="purple">
                 <header>
-                    <span>Entradas</span>
+                    <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
